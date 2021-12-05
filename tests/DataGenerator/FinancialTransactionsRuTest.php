5e7a56d7fe5f5dc67737206a50278d30c318002d
Dmitrii Sobinin <Sobalchik@yandex.ru>
2021-12-05 19:57:14 +0200

    Добавил 2 теста

--- tests/DataGenerator/FinancialTransactionsRuTest.php
+++ tests/DataGenerator/FinancialTransactionsRuTest.php
@@ -15,7 +15,40 @@ class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
 					'BankName' => '',
 					'BIC' => '',
 					'CorrespAcc' => '',
-				]
+				],
+			],
+			'filled with too long valuables' => [
+				[
+					'Name' => 'wgz!qq&9wkxpA5MYfd&gEp%tZC^u4k^Vw@u3S3eqSTNf#g9HdyV%@9gX5mBygAvjA4PK&4yZqmDHjC%cKxe!Cf3oCe%oDxEf$2TjdLNXDMtfnwVho6ewiRi#BFaZb5dJJHYiJ4#FdpXuwGPXSS4BD2%B#iT!mvFi1',
+					'PersonalAcc' => 'b^&23guvwB',
+					'BankName' => 'Mo@vtwbBRt8xEAud4cWzELYE2PQq5GJEYZmZwfLC@ddwYM',
+					'BIC' => 'V^nV%5Rh7Dsm2tvkiktAS',
+					'CorrespAcc' => 'V^nV%5Rh7Dsm2tvkiktAS',
+				],
+			],
+			'filled incorrectly' => [
+				[
+					'Name' => true,
+					'PersonalAcc' => true,
+					'BankName' => true,
+					'BIC' => true,
+					'CorrespAcc' => true,
+				],
+			],
+		];
+	}
+
+	public function getCorrectValidateFailSamples(): array
+	{
+		return [
+			'valid' => [
+				[
+					'Name' => 'Name',
+					'PersonalAcc' => 'PersonalAccount',
+					'BankName' => 'BankName',
+					'BIC' => 'BIC',
+					'CorrespAcc' => 'CorrespAcc',
+				],
 			],
 		];
 	}
@@ -36,33 +69,36 @@ class FinancialTransactionsRuTest extends \PHPUnit\Framework\TestCase
 		static::assertFalse($result->isSuccess());
 	}
 
-	public function testThatValidateSuccess(): void
+	/**
+	 * @dataProvider getCorrectValidateFailSamples
+	 *
+	 * @param array $fields
+	 */
+	public function testThatValidateSuccess(array $fields): void
 	{
 		$dataGenerator = new \App\DataGenerator\FinancialTransactionsRu();
 
-		$dataGenerator->setFields([]);
-
-		$dataGenerator
-			->setName('Name')
-			->setBIC('BIC')
-			->setBankName('BankName')
-			->setCorrespondentAccount('CorrespondentAccount')
-			->setPersonalAccount('CorrespondentAccount')
-		;
+		$dataGenerator->setFields($fields);
 
 		$result = $dataGenerator->validate();
 
 		static::assertTrue($result->isSuccess());
 	}
 
-	public function testGetData(): void
+	/**
+	 * @dataProvider getCorrectValidateFailSamples
+	 *
+	 * @param array $fields
+	 */
+	public function testGetData(array $fields): void
 	{
 		$dataGenerator = new \App\DataGenerator\FinancialTransactionsRu();
 
-		$dataGenerator->setFields([]);
+		$dataGenerator->setFields($fields);
 
 		$data = $dataGenerator->getData();
 
-		static::assertEquals('ST00012|Name=|PersonalAcc=|BankName=|BIC=|CorrespAcc=', $data);
+		static::assertEquals('ST00012|Name=Name|PersonalAcc=PersonalAccount|BankName=BankName|BIC=BIC|CorrespAcc=CorrespAcc',
+			$data);
 	}
 }
