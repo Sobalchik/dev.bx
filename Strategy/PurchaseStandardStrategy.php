0e1bd11b053a5a81a6f57b91fb88b3c3d2d8bd9e
Dmitrii Sobinin <Sobalchik@yandex.ru>
2021-12-15 07:18:54 +0200

    Добавлен StandardStrategy и простой Logger

---
+++ Strategy/PurchaseStandardStrategy.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Strategy;
+
+use Entity\Service;
+
+class PurchaseStandardStrategy implements PurchaseStrategy
+{
+	public function purchase(): Service
+	{
+		// take money
+		$service = new Service();
+
+		$service->setIsStandard(true);
+		$service->setActivatedUntil((new \DateTime())->modify("+ 30 days"));
+		\Event\EventBus::getInstance()->log((new \DateTime()));
+		$service->setType(Service::TYPES['standard']);
+		return $service;
+	}
+}
